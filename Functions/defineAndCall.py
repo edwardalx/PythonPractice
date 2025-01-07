bfde7056bd956a1cf528fb30077ad5e5ae4694d2
edwardalx <eobkwaku@gmail.com>
2025-01-07 01:09:06 +0000

    v1

---
+++ Functions/defineAndCall.py
@@ -0,0 +1,13 @@
+#import LoopsAndPasswords.EvenNumbers
+import push_Item_To_a_List
+
+def add_by_5(x):
+    return x+5
+print(add_by_5(6))
+
+def dividend(a,b = 1):     #default value of b is 1
+    result = a/b
+    print(result)
+dividend(6,4)
+
+print(push_Item_To_a_List.myList)
---
+++ Functions/push_Item_To_a_List.py
@@ -0,0 +1,9 @@
+myList = []
+
+def myFunction(x):
+    return 2*x -1
+
+for i in range(1,10):
+ myList.append(myFunction(i))   
+
+print(myList)
